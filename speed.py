9be8121887c46a4ea700d0fb96e8117310fb9254
江文 <jiangwen365@gmail.com>
2013-11-17 10:23:37 +0800

    Update and rename odbc_bench.py to speed.py

--- speed.py
+++ speed.py
@@ -1,52 +1,59 @@
 # -*- coding: utf-8 -*-
-'''
-import pypyodbc
-conn=pypyodbc.connect('dsn=mssql')
-cur = conn.cursor()
-cur.execute('select top 3 product_name from pyodbc_t')
-for row in cur.fetchall():
-    for field in row:
-        print field.decode('mbcs'),
-    print ''
 
-import sqlalchemy
-engine = sqlalchemy.create_engine('mssql+pypyodbc://mssql')
-for row in engine.execute('select top 2 product_name from pyodbc_t'):
-    for field in row:
-        print field.decode('mbcs'),
-    print ''
-    '''
 #                  Let Python load it's ODBC connecting tool pypyodbc
 
 from __future__ import print_function
-import os, os.path, time, sys
-from pypyodbc import win_create_mdb
+import os, os.path, time, sys, gc
+from pypyodbc121 import win_create_mdb
+
+if len(sys.argv) < 2:
+    usage = '''
+    usage: [the python interpreter] speed.py [the module to be benchmarked] 
+    for example:
+    
+    python speed.py pypyodbc
+    python speed.py pyodbc
+    pypy speed.py pypyodbc
+    ipy speed.py pypyodbc
+    '''
+    print (usage)
+    exit()
+    
+    
+localDir = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+temp_folder = localDir+'\\pypyodbc_mdb_test\\'
+if not os.path.exists(temp_folder):
+    os.mkdir(temp_folder)
 
 
-for x in range(120):   
+
+for x in range(4):   
     print ('file: '+str(x))
     #import pypyodbc_reuse_param as pypyodbc
     #import pyodbc as pypyodbc
-    if 'pyodbc' in sys.argv:
-        import pyodbc as pypyodbc
-        print ('Running with pyodbc %s' %pypyodbc.version)
-    else:
-        import pypyodbc as pypyodbc
-        print ('Running with pypyodbc %s' %pypyodbc.version)
+    
+    
+    pypyodbc = __import__(sys.argv[1])
+    #pypyodbc.pooling = False
+
+    print ('Running with pypyodbc %s' %pypyodbc.version)
     t_begin = time.time()
     fix = str(x%10)
-    if os.path.exists(u'D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'.mdb'):
-         os.remove(u'D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'.mdb')
-    if os.path.exists(u'D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'c.mdb'):
-         os.remove(u'D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'c.mdb')
-    if os.path.exists(u'D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'copy.mdb'):
-         os.remove(u'D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'copy.mdb')
-    win_create_mdb( u'D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'.mdb' )
+    if os.path.exists(temp_folder + u'PerformanceTest'+fix+'.mdb'):
+         os.remove(temp_folder + u'PerformanceTest'+fix+'.mdb')
+    if os.path.exists(temp_folder + u'PerformanceTest'+fix+'c.mdb'):
+         os.remove(temp_folder + u'PerformanceTest'+fix+'c.mdb')
+    if os.path.exists(temp_folder + u'PerformanceTest'+fix+'copy.mdb'):
+         os.remove(temp_folder + u'PerformanceTest'+fix+'copy.mdb')
+    win_create_mdb( temp_folder + u'PerformanceTest'+fix+'.mdb' )
     
     
-    conn = pypyodbc.connect(u'Driver={Microsoft Access Driver (*.mdb)};DBQ=D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'.mdb')
-    print (conn.getinfo(pypyodbc.SQL_DRIVER_NAME))
+    conn = pypyodbc.connect(u'Driver={Microsoft Access Driver (*.mdb)};DBQ='+temp_folder + u'PerformanceTest'+fix+'.mdb')
+
     cur = conn.cursor()
+    
+    
     cur.execute(u"""create table saleout (ID COUNTER PRIMARY KEY, customer_name varchar(255),
                             product_name varchar(255), 
                             price float, 
@@ -54,7 +61,7 @@ for x in range(120):
                             sell_time datetime);""")
     
     conn.commit()
-    for b in range(2500):
+    for b in range(5000):
         cur.executemany(u'''INSERT INTO saleout(customer_name,product_name,price,volume,sell_time) 
         VALUES(?,?,?,?,?)''',      [(u'杨天真','Apple IPhone 5','5500.1',1,'2012-1-21'),
                                     (u'郑现实','Huawei Ascend D2',None,1,'2012-1-21'),
@@ -71,24 +78,24 @@ for x in range(120):
 
     cur.commit()
     
-    cur.close()
+    #cur.close()
     
     
     
     conn.commit()
-    conn.close()
+    #conn.close()
     write_time = time.time() - t_begin
     print ('\tWrite time: '+str(write_time))
     
-    #pypyodbc.win_compact_mdb('D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'.mdb','D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'c.mdb')
+    #pypyodbc.win_compact_mdb('D:\\pypyodbc_mdb_test\\PerformanceTest'+fix+'.mdb','D:\\pypyodbc_mdb_test\\PerformanceTest'+fix+'c.mdb')
     
     t_begin = time.time()
-    conn = pypyodbc.connect(u'Driver={Microsoft Access Driver (*.mdb)};DBQ=D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'.mdb',unicode_results=True)
-    #conn = pypyodbc.win_connect_mdb('D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'.mdb')
+    conn = pypyodbc.connect(u'Driver={Microsoft Access Driver (*.mdb)};DBQ='+temp_folder + u'PerformanceTest'+fix+'.mdb',unicode_results=True)
+    #conn = pypyodbc.win_connect_mdb('D:\\pypyodbc_mdb_test\\PerformanceTest'+fix+'.mdb')
     cur = conn.cursor()
     
-    win_create_mdb( u'D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'copy.mdb' )
-    conn_copy = pypyodbc.connect(u'Driver={Microsoft Access Driver (*.mdb)};DBQ=D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'copy.mdb')
+    win_create_mdb( temp_folder + u'PerformanceTest'+fix+'copy.mdb' )
+    conn_copy = pypyodbc.connect(u'Driver={Microsoft Access Driver (*.mdb)};DBQ='+temp_folder + u'PerformanceTest'+fix+'copy.mdb')
 
 
     cur_copy = conn_copy.cursor()
@@ -112,16 +119,16 @@ for x in range(120):
         r_n +=4
     #cur_copy.close()
     cur_copy.commit()
-    conn_copy.close()
-    conn.close()
+    #conn_copy.close()
+    #conn.close()
     R_W_time = time.time() - t_begin
     print ('\tR & W time: '+str(time.time() - t_begin))
     
 
 
     t_begin = time.time()
-    conn = pypyodbc.connect(u'Driver={Microsoft Access Driver (*.mdb)};DBQ=D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'copy.mdb',unicode_results=True)
-    #conn = pypyodbc.win_connect_mdb('D:\\pypyodbc_mdb_test\\YourMDBfilepath'+fix+'.mdb')
+    conn = pypyodbc.connect(u'Driver={Microsoft Access Driver (*.mdb)};DBQ='+temp_folder + u'PerformanceTest'+fix+'copy.mdb',unicode_results=True)
+    #conn = pypyodbc.win_connect_mdb('D:\\pypyodbc_mdb_test\\PerformanceTest'+fix+'.mdb')
     cur = conn.cursor()
     
     cur.execute('select customer_name,product_name,price,volume,sell_time from saleout')
@@ -136,7 +143,12 @@ for x in range(120):
         r = cur.fetchmany(4)
         r_n +=4
     #cur_copy.close()
-    conn.close()
+    #conn.close()
     
     print ('\tRead  time: '+str(time.time() - t_begin))
+    
+    
+    print ("garbage collected:" + str(gc.collect()), end = ' ')
+    print ("garbage count:" + str(gc.garbage.__len__()), end = ' ')
+    print ("garbage:" + str(gc.garbage))
    
