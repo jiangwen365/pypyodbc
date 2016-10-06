e02d29d37aa8176d20055f0745b0987e300ac4b7
yuvalb <yuval@alooma.io>
2016-10-06 11:03:44 +0300

    Indentation fix"

--- pypyodbc.py
+++ pypyodbc.py
@@ -505,7 +505,7 @@ def UTF16_dec(buffer):
         ret = to_decode.decode(odbc_decoding)
     except UnicodeDecodeError:
         # We print as there is no logging here
-        print 'pypyodbc failed to decode "%s". Emitting in '
+        print 'pypyodbc failed to decode "%s". Emitting in ' \
               'raw form' % to_decode
         ret = to_decode
 
