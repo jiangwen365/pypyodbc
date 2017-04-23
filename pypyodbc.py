2e4eded97ad001c857d3c3e2dc88b3973717c8ca
dror <drorspace94@gmail.com>
2017-04-23 15:38:28 +0300

    Return convertor to integration repo

--- pypyodbc.py
+++ pypyodbc.py
@@ -25,7 +25,7 @@ pooling = True
 apilevel = '2.0'
 paramstyle = 'qmark'
 threadsafety = 1
-version = '1.3.1'
+version = '1.3.2'
 lowercase=True
 
 DEBUG = 0
@@ -646,8 +646,7 @@ SQL_data_type_dict = { \
     SQL_LONGVARBINARY   : (bytearray,           bytearray_cvt,              SQL_C_BINARY,       create_buffer,      20500 ,         True          ),
     SQL_BIGINT          : (long,                long,                       SQL_C_CHAR,         create_buffer,      150   ,         False         ),
     SQL_TINYINT         : (int,                 int,                        SQL_C_CHAR,         create_buffer,      150   ,         False         ),
-    SQL_BIT             : (int,                 lambda x:1 if x in BYTE_1 else 0,
-                           SQL_C_CHAR,         create_buffer,       2     ,        False         ),
+    SQL_BIT             : (bool,                lambda x:x == BYTE_1,       SQL_C_CHAR,         create_buffer,      2     ,         False         ),
     SQL_WCHAR           : (unicode,             lambda x: x,                SQL_C_WCHAR,        create_buffer_u,    2048  ,         False          ),
     SQL_WVARCHAR        : (unicode,             lambda x: x,                SQL_C_WCHAR,        create_buffer_u,    2048  ,         False          ),
     SQL_GUID            : (str,                 str,                        SQL_C_CHAR,         create_buffer,      2048  ,         False         ),
