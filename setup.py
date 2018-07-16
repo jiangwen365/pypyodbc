d403f09953aa61838f5ca4364a0416db457065b8
yuvalb <yuval@alooma.io>
2018-07-16 21:12:48 +0300

    upgrade pypyodbc

--- setup.py
+++ setup.py
@@ -7,12 +7,7 @@ import pypyodbc
 
 setup(
     name='pypyodbc',
-<<<<<<< HEAD
-    version='1.3.1',
-=======
     version=pypyodbc.version,
->>>>>>> forked/master
-    description='A Pure Python ctypes ODBC module',
     author='jiangwen365',
     author_email='jiangwen365@gmail.com',
     url='https://github.com/jiangwen365/pypyodbc',
@@ -20,26 +15,15 @@ setup(
     long_description="""
       A Pure Python ctypes ODBC module compatible with PyPy and almost totally same usage as pyodbc
       """,
-<<<<<<< HEAD
-      classifiers=[
-          "License :: OSI Approved :: MIT License",
-          "Programming Language :: Python",
-          "Development Status :: 4 - Beta",
-      ],
-      keywords='Python, Database, Interface, ODBC, PyPy',
-      license='MIT',
-      install_requires=[
-        'setuptools',
-        'python-dateutil',
-      ],
-      )
-=======
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Development Status :: 4 - Beta",
+      "License :: OSI Approved :: MIT License",
+      "Programming Language :: Python",
+      "Development Status :: 4 - Beta",
     ],
     keywords='Python, Database, Interface, ODBC, PyPy',
     license='MIT',
+    install_requires=[
+      'setuptools',
+      'python-dateutil',
+    ],
 )
->>>>>>> forked/master
